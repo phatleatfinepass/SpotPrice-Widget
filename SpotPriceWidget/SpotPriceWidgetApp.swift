287330726d9b8c3cbf997f8bbf9c7f24a203ff04
Phat Le <box.ryanle@gmail.com>
2026-08-25 21:57:15 +0300

    Initial Commit

---
+++ SpotPriceWidget/SpotPriceWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotPriceWidgetApp.swift
+//  SpotPriceWidget
+//
+//  Created by Chandler on 25.8.2026.
+//
+
+import SwiftUI
+
+@main
+struct SpotPriceWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
